996062432820f157721f2db43cdef7bd28c48153
hojun0010 <leehojun0010@gmail.com>
2024-05-17 11:06:48 +0900

    bj python practice ~144/145

--- js_practice/bj_1076.js
+++ js_practice/bj_1076.js
@@ -1,5 +1,4 @@
 const input = require('fs').readFileSync(process.platform === "linux"?0:"./js_practice/input.txt","utf-8").toString().split('\n').map((e) => e.trim());
-console.log(input);
 
 let arr = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"];
 console.log((10*arr.indexOf(input[0])+arr.indexOf(input[1]))*Math.pow(10,arr.indexOf(input[2])));
